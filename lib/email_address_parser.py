70b2327dbf71fe3d1dae689c0fffdc462ad5b5a7
Steven McVey <steven@stevenmcvey.com>
2024-02-27 13:00:03 -0500

    lab complete

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,17 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+
+  def __init__(self, str):
+    self.email_addresses = str
+
+  def parse(self):
+    print(self.email_addresses)
+    split_pattern = re.compile(r"\s|,\s?")
+    emails = sorted(split_pattern.split(self.email_addresses))
+    print(emails)
+
+    validate_pattern = re.compile(r'[\w.]*@\w*')
+    valid_emails = [email for email in emails if validate_pattern.match(email)]
+    print(valid_emails)
+    return valid_emails
